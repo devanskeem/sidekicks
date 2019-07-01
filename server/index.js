92f7197496cf636b0fce292710da9d7f3d18a8f4
Devan Skeem <devanskeem@gmail.com>
2019-07-01 13:26:17 -0600

    feat: adding auth0 universal login

--- server/index.js
+++ server/index.js
@@ -2,20 +2,39 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
-const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
-const app = express()
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const Auth0Strategy = require('passport-auth0')
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING, FRONTEND_URL, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_DOMAIN } = process.env
 
+const app = express()
 app.use(express.json())
+app.use(bodyParser.json())
 app.use(session({
     secret: SESSION_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-        max_age: 60 * 60 * 1000
-    }
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
 }))
-
-
+// var strategy = new Auth0Strategy(
+//     {
+//       domain: AUTH0_DOMAIN,
+//       clientID: AUTH0_CLIENT_ID,
+//       clientSecret: AUTH0_CLIENT_SECRET,
+//       callbackURL:
+//         process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+//     },
+//     function (accessToken, refreshToken, extraParams, profile, done) {
+//       // accessToken is the token to call Auth0 API (not needed in the most cases)
+//       // extraParams.id_token has the JSON Web Token
+//       // profile has all the information from the user
+//       return done(null, profile);
+//     }
+//   );
+  
+  passport.use(strategy);
+app.use(passport.session())
+app.use(passport.initialize())
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db)
     console.log('database set')
@@ -54,3 +73,37 @@ massive(CONNECTION_STRING).then(db => {
 app.listen(SERVER_PORT, () => {
     console.log('listening on port: ', SERVER_PORT)
 })
+
+
+ // Endpoints to authentciate (we'll hit these with axios on our front end)
+// app.get(
+//     "/auth/google",
+//     passport.authenticate("auth0", {
+//         scope: ["profile", "email"],
+//         prompt: "select_account"
+//     })
+// );
+// // call back url
+// app.get(
+//     "/auth/callback",
+//     passport.authenticate("auth0", {
+//         // this redirects the user back to the front end where they started the login process
+//         successRedirect: `${FRONTEND_URL}/#/browse-events`
+//         // user the hash symbol above because we are using Hashrouter
+//     })
+// );
+
+// app.get("/auth/user", (req, res) => {
+//     if (req.user) {
+//         res.status(200).send(req.user);
+//     } else {
+//         res.status(401).send("Unauthorized User");
+//     }
+// });
+
+// app.get("/auth/logout", (req, res) => {
+//     req.logOut();
+
+//     // this is a built in method in passport that kills the session and resets the user property
+//     res.redirect(process.env.FRONTEND_URL);
+// });
