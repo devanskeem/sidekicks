90c5eb0553f1a2b67a7a75e9a11b9d6f7bf447e0
Devan Skeem <devanskeem@gmail.com>
2019-07-01 10:43:06 -0600

    feat: planning out endpoints

--- server/index.js
+++ server/index.js
@@ -21,6 +21,36 @@ massive(CONNECTION_STRING).then(db => {
     console.log('database set')
 })
 
+//event endpoints
+
+//app.get('/events/all', event_ctrl.getAllEvents) response: [{eventId: int, eventTitle: string, eventDescription: string, eventHost: {user}, attendees: [users}]
+//app.put('/events/update/:id, event_ctrl.updateEvent) response: updated {event}
+//app.delete('/events/delete/:id', event_ctrl.deleteEvent) response: status code
+//app.post('/events/add', event_ctrl.addEvent) response: newly created {event}
+
+//message endpoints
+
+//app.get('/messages/:event-id', event_ctrl.getMessagesByEvent) response: [{messageId: int, user: {user}, content: string, timestamp}]
+//app.delete('/messages/delete/:id', event_ctrl.deleteMessageById) response: status code
+//app.delete('/messages/delete/:event-id', event_ctrl.deleteMessageByEvent) response: status code
+//app.post('/messages/add', event_ctrl.addMessage) response: newly created {message}
+
+//user endpoints 
+
+//app.get('/users/all', user_ctrl.getAllUsers) response: [{userId: int, username: string, firstName: string, lastName: string, age: integer, location: string, gender: string, image: string,bio: string}]
+//app.get('/users/current', user_ctrl.getCurrentUser)
+//app.get('/users/:id', user_ctrl.getUserById) response: {user} 
+//app.get('/users/:event-id', user_ctrl.getUserByEvent) response: [{user}]
+//app.post('/users/add', user_ctrl.addUser) response newly create {user}
+//app.put('/users/update' user_ctrl.updateUser)
+
+
+//auth endpoint
+
+//app.post('/auth/login', auth_ctrl.login)
+//app.post('/auth/register', auth_ctrl.register)
+//app.get('/auth/logout', auth_ctrl.logout)
+
 app.listen(SERVER_PORT, () => {
     console.log('listening on port: ', SERVER_PORT)
 })
