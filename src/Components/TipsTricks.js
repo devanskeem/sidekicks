a0f6109f3f1c909ce6c4474f52a1ff9399558936
Caleb Scoville <calebscoville2010@gmail.com>
2019-07-02 10:25:02 -0600

    Updated Router.js; created and finished UserProfile

--- src/Components/TipsTricks.js
+++ src/Components/TipsTricks.js
@@ -30,22 +30,42 @@ import {
               <li>2: please only post appropriate pictures; you are a valuable member of our community and we want you to stay with us.</li>
               <br /> 
               <li>3: select tags that you're interested in. they can be things you like to do, or things you want to do.</li>
-              <br /> 
+              <br />
+              <li>4: consider the following ideas to put in your bio to make hosts more likely to reach out to you: put two truths and a lie in there; list a song that describes your life; list the pros and cons about your life in a fun way; share some unpopular opinions; talk about your VERY favorite things</li>
+              <link href="https://blog.photofeeler.com/best-tinder-bios-profile-tips/">more ideas for making your bio BOMB!</link>
+              <br />
               <h1>attending events</h1>
               <br />
               <li>1: be safe, especially as a female. if it's a public place, use your best judgement. if it's someone's home or a secluded area, wait for another person to arrive or plan to attend with a friend or have a friend accompany you.</li>
               <br /> 
-              <li>2: be yourself, but be your besgt self. please practice good manners and hygiene.</li>
+              <li>2: be yourself, but be your best self. please practice good manners and hygiene.</li>
+              <br />
+              <li>3: please rate the event afterwards. this helps us and other members know more about the kinds of events the host creates.</li>
               <br />
               <h1>creating events</h1>
               <br />
               <li>1: please take the time to think about the event before creating it. a well-thought-out well-attended event is better than a poorly-thought-out cancelled-due-to-low-sign-up event.</li>
               <br /> 
               <li>2: include as many details as possible. it can be annoying to have to answer a lot of messages when you could have avoided that by creating a more thorough listing.</li>
+              <br />
+              <li>2: title: introduce event and encourage others to come.</li>
+              <br />
+              <li>2: the photo: give participants a feel for what to expect and make sure it's appropriate (or we'll ban you:).</li>
+              <br />
+              <li>2: cost: if you're expecting participants to contribute, you need to specify the amount and how you'd like to receive it (e.g. Venmo, cash, check, PayPal, gold bars, ivory).</li>
+              <br />
+              <li>2: include as many details as possible. it can be annoying to have to answer a lot of messages when you could have avoided that by creating a more thorough listing.</li>
+              <br />
+              <li>2: include as many details as possible. it can be annoying to have to answer a lot of messages when you could have avoided that by creating a more thorough listing.</li>
+              <br />
+              <li>2: include as many details as possible. it can be annoying to have to answer a lot of messages when you could have avoided that by creating a more thorough listing.</li>
+              <br />
+              <li>2: include as many details as possible. it can be annoying to have to answer a lot of messages when you could have avoided that by creating a more thorough listing.</li>
               <br /> 
               <h1>examples of events</h1>
               <br />
-              <li>go to this list of hobbies to get some ideas of different types of events you could host: ______</li>
+              <li>go to this list of hobbies to get some ideas of different types of events you could host:</li>
+              <link href="https://en.wikipedia.org/wiki/List_of_hobbies">a comprehensive list of hobbies and ideas for events</link> 
               <br />
               <li>here are some of our ideas:</li>
               <br />
