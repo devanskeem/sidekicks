447838bd7f73cb00758c6d0b9370450d908b9672
Melody Vincent <melody.vincent@zoho.com>
2019-07-05 10:14:40 -0600

    started coding MyEvents.js

--- server/index.js
+++ server/index.js
@@ -88,7 +88,7 @@ app.get('/events/all', event_ctrl.getAllEvents) //response: [{eventId: int, even
 
 app.get('/events/:id', event_ctrl.getEventById)
 app.get('/events/user', event_ctrl.getEventsByUser)
-app.put('/events/update/:id', event_ctrl.updateEvent) 
+app.put('/events/update/:id', event_ctrl.updateEvent)
 app.delete('/events/delete/:id', event_ctrl.deleteEvent) //response: status code
 app.post('/events/add', event_ctrl.addEvent) //response: newly created {event}
 
--- src/Components/Events/MyEvents.js
+++ src/Components/Events/MyEvents.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react'
+import Axios from 'axios';
 
 export class MyEvents extends Component {
+constructor(){
+  super();
+  this.state = {
+    event: [],
+    user: '',
+  };
+}
+
+componentDidMount(){
+  axios.get('/events/user').then
+  (response => {
+    this.props.getUser
+    (response.data);
+  });
+
+  axios.get('api/')
+}
+
+
+
+
+
   render() {
     return (
       <div>
