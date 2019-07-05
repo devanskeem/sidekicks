e60aa3fb75608405f041b9f50e0aead6be272214
detanaded <48308236+detanaded@users.noreply.github.com>
2019-07-05 11:02:32 -0600

    Feature: Styling Landing
    Added Skeletal Structure for a potential Landing page.

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Form, Form2, Join, Loginbtn, Test, Test2,Title, Phrase} from './Styled'
+
+
+
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <Test >
+        <Test2>
+          <Title>sidekicks</Title>
+          <Phrase>social event organizer</Phrase>
+        </Test2>
+        <Form>
+          <Form2>
+          <Join>sign up</Join>
+          <p>Already a Member? <Loginbtn>Log in</Loginbtn></p>
+          </Form2>
+        </Form>
+      </Test>
+    )
+  }
+}
+
+
