ed67333e5313c00ec1c1e92d640e13ddc1b60bf2
detanaded <48308236+detanaded@users.noreply.github.com>
2019-07-05 12:21:42 -0600

    Feature: Added Navbar
    Displaying Navbar only after user has signed in using props.location.pathname.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,34 +1,27 @@
-// import React from 'react'
-// import 'reset-css'
-// import axios from 'axios'
-// import {Link} from 'react-router-dom'
-// import {connect} from 'react-redux'
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-// import {
-//     nav,
-//     AppContainer,
-//     FormContainer,
-//     Span
-//   } from './AppStylesNav';
+function Navbar(props) {
+  console.log(props);
+  if (props.location.pathname === "/") return null;
+  else {
+    return (
+      <nav>
+        <Link className="browse" to="/browseevents">
+          Events
+        </Link>
+        <Link className="myevents" to="/myevents">
+          My Events
+        </Link>
+        <Link className="messages" to="/messages">
+          Messages
+        </Link>
+        <Link className="userprofile" to="/userprofile">
+          Profile
+        </Link>
+      </nav>
+    );
+  }
+}
 
-// const Navbar = ({ username }) => {
-//     return (
-//         <AppContainer>
-
-//             <Span><Link to='/eventdisplay' style={{ textDecoration: 'none', color: '#5e0a0a' }}>events</Link></Span>
-//             <Span><Link to='/messages' style={{ textDecoration: 'none', color: '#5e0a0a'  }}>messages</Link></Span>
-//             <Span><Link to='/userprofile' style={{ textDecoration: 'none', color: '#5e0a0a'  }}>my profile</Link></Span>
-//             <Span><Link to='/tipstricks' style={{ textDecoration: 'none', color: '#5e0a0a'  }}>tips&tricks</Link></Span>
-                      
-//             <span>{username && username}</span>
-//         </AppContainer>
-//     )
-// }
-
-// const mapStateToProps = (reduxState) => {
-//     return {
-//         username: reduxState.username
-//     }
-// }
-
-// export default connect(mapStateToProps)(Navbar)
+export default withRouter(Navbar);
