19ad0664f3e4e7110d14c45ac327c101d6ecc219
detanaded <48308236+detanaded@users.noreply.github.com>
2019-07-05 22:14:42 -0600

    Additional Landing page Styling
    Added Responsive styling from tablet up.

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,60 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { Form, Form2, Join, Loginbtn, Test, Test2,Title, Phrase, P, Signup} from './Styled'
-
-
-
-
-
+import {
+  Form,
+  Form2,
+  Join,
+  Loginbtn,
+  Test,
+  Test2,
+  Title,
+  Phrase,
+  P,
+  Signup,
+  Nav,
+  Hero,
+  TitleDiv,
+} from "./Styled";
 
 export default class Landing extends Component {
   render() {
     return (
-      <Test >
-
-        {/* <Test2>*/}
-        <div>
-          <div className="logo">
-            <a
-            id="loginanimation"
-            className='login animated slideOutDown'
-
-            >
-             
-             <img
-               alt=""
-               src={sidekick}
-               style={{
-                 height:"300px",
-                 margin: "70px",
-                 position: "center",
-                 top: "50px",
-                 left: "60px"
-               }}
-             />
-
-            </a>
-
-          </div>
-        </div>
+      <Test>
+        <Nav>
+          <TitleDiv>
+            <Title>sidekicks</Title>
+          </TitleDiv> 
+        </Nav>
+        <Hero>
+          Hello
+        </Hero>
+        <Test2>
+          <Title>sidekicks</Title>
+          <Phrase>social event organizer</Phrase>
+        </Test2>
         <Form>
           <Form2>
-          <Join>
-          <Signup href="http://localhost:3333/auth/google">sign up</Signup>
-          </Join>
-          <P>
-            Already Signup Member? 
-            <Loginbtn>
-              <Signup href="http://localhost:3333/auth/google">
-                Login
-              </Signup>
-            </Loginbtn>
-          </P>
+            <Join>
+              <Signup href="http://localhost:3333/auth/google">sign up</Signup>
+            </Join>
+            <P>
+              Already a Member?
+              <Loginbtn>
+                <Signup href="http://localhost:3333/auth/google">Login</Signup>
+              </Loginbtn>
+            </P>
           </Form2>
         </Form>
       </Test>
-    )
+    );
   }
 }
-
-
