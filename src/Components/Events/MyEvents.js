4efad74360f45823a5860bd466fc584eb57406e3
Devan Skeem <devanskeem@gmail.com>
2019-07-09 12:02:07 -0600

    feat: fixed join event

--- src/Components/Events/MyEvents.js
+++ src/Components/Events/MyEvents.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import {updateCurrEvent} from '../../redux/reducer'
-import {connect} from 'react-redux'
+import { updateCurrEvent } from '../../redux/reducer'
+import { connect } from 'react-redux'
 class MyEvents extends Component {
 
   constructor() {
@@ -36,58 +36,63 @@ class MyEvents extends Component {
   };
 
   render() {
-    const hostedEventDisplay = this.state.hostedEvents.map((event, i) => {
-      return (
-        <div onClick={() => this.toggleEventDisplay(event.event_id)} key={i} style={{
-          border: '1px solid black',
-          width: '200px'
-        }}>
-          <img src={event.image} />
-          <p>{event.name}</p>
-          <p>{event.description}</p>
-          <p>{event.location}</p>
-          <p>Cost: {event.cost}</p>
-        </div>
-      )
-    })
-
-    const JoinedEventDisplay = this.state.joinedEvents.map((event, i) => {
-      return (
-        <div onClick={() => this.toggleEventDisplay(event.event_id)} key={i} style={{
-          border: '1px solid black',
-          width: '200px'
-        }}>
-          <img src={event.image} />
-          <p>{event.name}</p>
-          <p>{event.description}</p>
-          <p>{event.location}</p>
-          <p>Cost: {event.cost}</p>
-        </div>
-      )
-    })
-
+    let hostedEventDisplay = null
+    if (this.state.hostedEvents) {
+      hostedEventDisplay = this.state.hostedEvents.map((event, i) => {
+        return (
+          <div onClick={() => this.toggleEventDisplay(event.event_id)} key={i} style={{
+            border: '1px solid black',
+            width: '200px'
+          }}>
+            <img src={event.image} />
+            <p>{event.name}</p>
+            <p>{event.description}</p>
+            <p>{event.location}</p>
+            <p>Cost: {event.cost}</p>
+          </div>
+        )
+      })
+    }
+    let joinedEventDisplay = null
+    if (this.state.joinedEvents) {
+       joinedEventDisplay = this.state.joinedEvents.map((event, i) => {
+         console.log('event', event)
+        return (
+          <div onClick={() => this.toggleEventDisplay(event.event_id)} key={i} style={{
+            border: '1px solid black',
+            width: '200px'
+          }}>
+            <img src={event.image} />
+            <p>{event.name}</p>
+            <p>{event.description}</p>
+            <p>{event.location}</p>
+            <p>Cost: {event.cost}</p>
+          </div>
+        )
+      })
+    }
     return (
       <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-around'
-      }}
-      >
-        <div
         style={{
           display: 'flex',
-          flexDirection: 'column'
+          justifyContent: 'space-around'
         }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column'
+          }}
         >
           JoinedEventDisplay
-          {JoinedEventDisplay}
+          {joinedEventDisplay}
 
         </div>
         <div
-         style={{
-          display: 'flex',
-          flexDirection: 'column'
-        }}
+          style={{
+            display: 'flex',
+            flexDirection: 'column'
+          }}
         >
           hostedEventDisplay
           {hostedEventDisplay}
@@ -99,9 +104,9 @@ class MyEvents extends Component {
 
 }
 
-function mapStateToProps(reduxState){
+function mapStateToProps(reduxState) {
   return reduxState
 }
 
-const mapDispatchToProps = {updateCurrEvent}
+const mapDispatchToProps = { updateCurrEvent }
 export default connect(mapStateToProps, mapDispatchToProps)(MyEvents);
